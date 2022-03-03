7c88f79f05f239405db241c9b0777645ef372cbc
Alexander Davis <talexanderd99@gmail.com>
2022-03-02 20:13:57 -0600

    Handled case where stickynotes can't be attached due to the element not accepting span children. Instead created noteholder as a child of the parent element, and positioned it at the stuckOn element's relative offset from parent.

--- DOMHopper/src/Stickynote.js
+++ DOMHopper/src/Stickynote.js
@@ -6,24 +6,62 @@ import {
 } from "./helpers.js";
 import { PROHIBIT_SELECTION } from "./constants.js";
 
+const elementsRejectingSpanChildren = ["INPUT"];
+function canHaveSpanChildren(element) {
+  return !elementsRejectingSpanChildren.includes(element.nodeName);
+}
+
 export default class Stickynote {
   constructor(element, text, noteClass = "registerNote") {
     this.stuckOn = element;
     this.noteClass = noteClass;
-    var hasNoteHolder =
-      getChildrenWithClass(this.stuckOn, "noteHolder").length > 0;
-    if (!hasNoteHolder) {
-      var elementIsPositioned = this.stuckOn.style.position != "";
-      console.log("stuckOn style.position", this.stuckOn.style.position);
-      console.log("elementIsPositioned", elementIsPositioned);
-      if (!elementIsPositioned) this.addInertPositioning(this.stuckOn);
-      this.noteHolder = makeSpan("", this.stuckOn);
-      this.noteHolder.setAttribute("class", "noteHolder");
-      this.noteHolder.setAttribute(PROHIBIT_SELECTION, "");
-    } else this.noteHolder = getChildWithClass(element, "noteHolder");
-    this.note = makeSpan(text, this.noteHolder);
-    this.note.setAttribute("class", this.noteClass);
-    this.note.setAttribute(PROHIBIT_SELECTION, "");
+    console.log("stuck on", element);
+    console.log("node name", element.nodeName);
+    console.log("can have span kids ", canHaveSpanChildren(element));
+    if (canHaveSpanChildren(this.stuckOn)) {
+      var hasNoteHolder =
+        getChildrenWithClass(this.stuckOn, "noteHolder").length > 0;
+      if (!hasNoteHolder) {
+        var elementIsPositioned = this.stuckOn.style.position != "";
+        console.log("stuckOn style.position", this.stuckOn.style.position);
+        console.log("elementIsPositioned", elementIsPositioned);
+        if (!elementIsPositioned) this.addInertPositioning(this.stuckOn);
+        this.noteHolder = makeSpan("", this.stuckOn);
+        this.noteHolder.setAttribute("class", "noteHolder");
+        this.noteHolder.setAttribute(PROHIBIT_SELECTION, "");
+      } else this.noteHolder = getChildWithClass(element, "noteHolder");
+      this.note = makeSpan(text, this.noteHolder);
+      this.note.setAttribute("class", this.noteClass);
+      this.note.setAttribute(PROHIBIT_SELECTION, "");
+    } else {
+      var parentElement = this.stuckOn.parentElement;
+
+      var hasNoteHolder =
+        getChildrenWithClass(parentElement, "noteHolder").length > 0;
+      if (!hasNoteHolder) {
+        var elementIsPositioned = parentElement.style.position != "";
+        if (!elementIsPositioned) this.addInertPositioning(parentElement);
+        this.noteHolder = makeSpan("", parentElement);
+        this.noteHolder.setAttribute("class", "noteHolder");
+        this.noteHolder.setAttribute(
+          "style",
+          "position: absolute; top: " +
+            this.stuckOn.offsetTop +
+            "px; left: " +
+            this.stuckOn.offsetLeft +
+            "px;"
+        );
+        console.log(
+          "offset of stuckOn ",
+          this.stuckOn.offsetTop,
+          this.stuckOn.offsetLeft
+        );
+        this.noteHolder.setAttribute(PROHIBIT_SELECTION, "");
+      } else this.noteHolder = getChildWithClass(parentElement, "noteHolder");
+      this.note = makeSpan(text, this.noteHolder);
+      this.note.setAttribute("class", this.noteClass);
+      this.note.setAttribute(PROHIBIT_SELECTION, "");
+    }
   }
 
   addInertPositioning(element) {
@@ -42,8 +80,11 @@ export default class Stickynote {
     var hasNotesLeft =
       getChildrenWithClass(this.noteHolder, this.noteClass).length > 0;
     if (!hasNotesLeft) {
+      var elementToRestorePositioning = canHaveSpanChildren(this.stuckOn)
+        ? this.noteHolder
+        : this.noteHolder.parentElement;
+      this.removeInertPositioning(elementToRestorePositioning);
       removeNode(this.noteHolder);
-      this.removeInertPositioning(this.stuckOn);
     }
   }
 }
